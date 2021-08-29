c207825b27f0d06d7f8e6030316cbd811e0d464b
Alzbeta Myskova <betka.myskova@seznam.cz>
2021-08-29 12:17:08 +0200

    upraveno stylování na dynamické css

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -25,16 +25,12 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+      <div className={!isValid ? "form-control invalid" : "form-control"}>
+        <label>Course Goal</label>
         <input
-          style={{
-            borderColor: !isValid ? 'red' : '#ccc',
-            background: !isValid ? 'salmon' : 'transparent'
-            }}
           type="text"
           onChange={goalInputChangeHandler}
-          />
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
